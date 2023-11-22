71a5842bdce0f9e4e48b187c285c45a00f26385e
Valeriia295 <sokolsokol295@gmail.com>
2023-11-22 20:40:45 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,26 +4,46 @@ const form = document.querySelector('form.feedback-form');
 const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 
-form.addEventListener(
-  'input',
-  throttle(function () {
-    const inputData = {
+let data = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+reloadThePage();
+
+form.addEventListener('input', function () {
+  storedInLocalStorage();
+});
+
+function reloadThePage() {
+  if (data) {
+    email.value = data.email || '';
+    message.value = data.message || '';
+  }
+}
+
+const handleSubmit = event => {
+  event.preventDefault();
+  if (email.value !== '' || message.value !== '') {
+    console.log({
+      email: email.value,
+      message: message.value,
+    });
+  }
+
+  if (email.value === '' || message.value === '') {
+    return alert('Будь ласка, заповніть всі поля');
+  }
+
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+  data = {};
+};
+
+form.addEventListener('submit', handleSubmit);
+
+const storedInLocalStorage = throttle(() => {
+  if (email.value !== '' || message.value !== '') {
+    data = {
       email: email.value,
       message: message.value,
     };
-    localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
-  }),
-  500
-);
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const formInput = {
-    email: email.value,
-    message: message.value,
-  };
-  console.log(formInput);
-  localStorage.removeItem('feedback-form-state');
-  email.value = '';
-  message.value = '';
-});
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
