92645ab608b986ed178c8e55f5acba5f6303ff24
Valeriia295 <sokolsokol295@gmail.com>
2023-11-22 21:36:56 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,28 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const time = localStorage.getItem('videoplayer-current-time');
 
-player
-  .setCurrentTime(time)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+const currentTime = time => {
+  const seconds = time.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.on('timeupdate', throttle(currentTime, 1000));
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000)
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0
 );
